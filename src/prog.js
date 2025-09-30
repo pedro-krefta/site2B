02b2104a37fe7fb8188598eb04733b7bd9b27bf1
krefta <pedro_hk_duarte@estudante.sesisenai.org.br>
2025-09-30 11:06:38 -0300

    v3

---
+++ src/prog.js
@@ -0,0 +1,9 @@
+particlesJS("particles-js", {
+    particles: {
+      number: { value: 100 },
+      size: { value: 3 },
+      move: { speed: 2 },
+      line_linked: { enable: true },
+      color: { value: "#ffffff" }
+    },
+  });
