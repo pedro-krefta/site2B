bcfca73a4f3dd6c83c82f51b842e5a70a9fcab24
pedro-krefta <pedro_hk_duarte@estudante.sesisenai.org.br>
2025-10-05 02:59:46 -0300

    v4.4

--- src/prog.js
+++ src/prog.js
@@ -1,9 +1,16 @@
-particlesJS("particles-js", {
+document.addEventListener("DOMContentLoaded", () => {
+  particlesJS("particles-js", {
     particles: {
       number: { value: 80 },
-      size: { value: 3 },
+      size: { value: 1 },
       move: { speed: 2 },
       line_linked: { enable: false },
       color: { value: "#ffffff" }
-    },
+    }
   });
+
+  AOS.init({
+    duration: 1000,
+    once: true,
+  });
+});
